4d0822c92cb6ef6055a22f33d3250c524a8d05a4
David Walker <dave@radicaldave.com>
2021-03-23 14:49:09 -0500

    init

---
+++ src/Type.cs
@@ -0,0 +1,13 @@
+﻿namespace Shared.Extensions
+{
+    public static class Type
+    {
+        public static void SetProperty(this System.Type type, string name, object obj, object value)
+        {
+            if (name == null || obj == null || value == null) return;
+            var property = type.GetProperty(name);
+            if (property == null) return;
+            property.SetValue(obj, value, null);
+        }
+    }
+}
